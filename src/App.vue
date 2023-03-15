ffb223f3ddc42074bb514f7a555775052a71e792
Şehriban Turan <107572130+sehribanturan@users.noreply.github.com>
2023-03-15 11:23:16 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<fastcomponent></fastcomponent>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
+
+import fastcomponent from './components/fastcomponent.vue';
 export default {
   name: "App",
   components: {
-    HelloWorld,
-  },
+  fastcomponent
+
+},
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
